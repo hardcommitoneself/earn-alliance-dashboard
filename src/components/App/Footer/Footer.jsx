64215b07abcfd4bbf7f9b68940181da10edc8998
hardcommitoneself <hardcommitoneself@gmail.com>
2023-02-07 00:27:44 -0800

    feat: add Footer

--- src/components/App/Footer/Footer.jsx
+++ src/components/App/Footer/Footer.jsx
@@ -1,7 +1,90 @@
 import React from "react";
+import * as EA from "./styles";
+
+const pages = [
+  {
+    name: "Games",
+    url: "/games",
+  },
+  {
+    name: "News",
+    url: "/news",
+  },
+  {
+    name: "Allies",
+    url: "/allies",
+  },
+  {
+    name: "Badges",
+    url: "/badges",
+  },
+  {
+    name: "Whitepaper",
+    url: "/whitepaper",
+  },
+];
+
+const socials = [
+  {
+    url: "https://twitter.com/xxx",
+    icon: "images/twitter.svg",
+  },
+  {
+    url: "https://facebook.com/yyy",
+    icon: "images/facebook.svg",
+  },
+  {
+    url: "https://discord.com/zzz",
+    icon: "images/discord.svg",
+  },
+];
+
+const terms = ["Privacy Policy", "Terms of Use", "Cookies Policy"];
 
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <EA.Footer>
+      {/* border */}
+      <EA.Border>
+        <EA.BorderLeftOverlay />
+        <EA.BorderRightOverlay />
+      </EA.Border>
+
+      <EA.FooterInner>
+        <EA.FooterTopContainer>
+          <EA.FooterLogo src="images/logo.svg" alt="logo" />
+
+          <EA.FooterMenuContainer>
+            <EA.FooterAppMenuContainer>
+              {pages.map((page, index) => (
+                <EA.FooterAppMenuItem key={index} href={page.url}>
+                  {page.name}
+                </EA.FooterAppMenuItem>
+              ))}
+            </EA.FooterAppMenuContainer>
+
+            <EA.FooterSocialMenuContainer>
+              {socials.map((social, index) => (
+                <EA.FooterSocialMenuItem key={index}>
+                  <EA.FooterSocialImage src={social.icon} alt="social" />
+                </EA.FooterSocialMenuItem>
+              ))}
+            </EA.FooterSocialMenuContainer>
+          </EA.FooterMenuContainer>
+        </EA.FooterTopContainer>
+
+        <EA.FooterBottomContainer>
+          <EA.CopyRight>All Rights Reserved 2022 Earn Alliance</EA.CopyRight>
+
+          <EA.FooterTermsContainer>
+            {terms.map((term, index) => (
+              <EA.FooterTermsItem key={index}>{term}</EA.FooterTermsItem>
+            ))}
+          </EA.FooterTermsContainer>
+        </EA.FooterBottomContainer>
+      </EA.FooterInner>
+    </EA.Footer>
+  );
 };
 
 export default Footer;
---
+++ src/components/App/Footer/styles.js
@@ -0,0 +1,133 @@
+import styled from "styled-components";
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Border = styled.div`
+  position: relative;
+  height: 1px;
+  background-image: linear-gradient(
+    to right,
+    transparent,
+    #eed5a5,
+    transparent
+  );
+`;
+
+export const BorderLeftOverlay = styled.div`
+  position: absolute;
+  width: 33.3333%;
+  height: 100%;
+  background-image: linear-gradient(to right, black, transparent);
+`;
+
+export const BorderRightOverlay = styled.div`
+  position: absolute;
+  width: 33.3333%;
+  height: 100%;
+  background-image: linear-gradient(to left, black, transparent);
+  right: 0;
+`;
+
+export const FooterInner = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 44px;
+  padding-left: 200px;
+  padding-right: 200px;
+  padding-top: 58px;
+  padding-bottom: 58px;
+  background-color: ${(props) => props.theme.colors.primary};
+`;
+
+export const FooterTopContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const FooterBottomContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FooterLogo = styled.img`
+  cursor: pointer;
+
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 200ms;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
+
+export const FooterMenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 54px;
+`;
+
+export const FooterAppMenuContainer = styled.div`
+  display: flex;
+  gap: 48px;
+  margin-top: -5px;
+`;
+
+export const FooterSocialMenuContainer = styled.div`
+  display: flex;
+  gap: 24px;
+  cursor: pointer;
+`;
+
+export const FooterAppMenuItem = styled.a`
+  font-size: 18px;
+  line-height: 20px;
+  font-weight: 400;
+  color: white;
+  text-decoration: none;
+
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 200ms;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
+
+export const FooterSocialMenuItem = styled.a`
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 200ms;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
+
+export const FooterSocialImage = styled.img``;
+
+export const CopyRight = styled.span`
+  font-size: 18px;
+  line-height: 120%;
+  color: white;
+  opacity: 0.3;
+  font-weight: 400;
+`;
+
+export const FooterTermsContainer = styled.div`
+  display: flex;
+  gap: 54px;
+`;
+
+export const FooterTermsItem = styled.span`
+  font-size: 18px;
+  line-height: 120%;
+  font-weight: 400;
+  color: white;
+`;
